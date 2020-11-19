5689f52e9f94ad8a72a5c44b8573b64068e29351
CarlosBonfim <brunobonfim10@hotmail.com>
2020-11-19 09:49:05 -0400

    adicionando estilos

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <h1>Problema de Montyhall</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+  body{
+    color: #ffffff;
+    background: linear-gradient(to right, #22c1c3, #fdbb2d);
+  }
+  #app{
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+  }
+  #app{
+    /* border: 1px solid #000; */
+    border-radius: 3px;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+</style>
